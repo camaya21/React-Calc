c81ac482a2cecaec771f3d1cfc22b27355f3f7d0
camaya21 <cindymamaya21@gmail.com>
2023-05-11 10:25:46 -0400

    Basic css added

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,6 +1,8 @@
+import "./styles.css"
+
 function App() {
   return (
-    <div className="claculator-grid">
+    <div className="calculator-grid">
       <div className="output">
         <div className="previous-operand"></div>
         <div className="current-operand"></div>
