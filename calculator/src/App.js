364006607daeadd307439f51302f7774fec48891
camaya21 <cindymamaya21@gmail.com>
2023-05-17 15:00:10 -0400

    Added integer formatter

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -124,6 +124,7 @@ const INTEGER_FORMATTER = new Intl.NumberFormat("en-us", {
 function formatOperand(operand) {
   if (operand == null) return
   const [integer, decimal] = operand.split('.')
+  if (decimal == null ) return INTEGER_FORMATTER.format(integer)
 }
 
 function App() {
@@ -132,8 +133,8 @@ function App() {
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand">{previousOperand} {operation}</div>
-        <div className="current-operand">{currentOperand}</div>
+        <div className="previous-operand">{formatOperand(previousOperand)} {operation}</div>
+        <div className="current-operand">{formatOperand(currentOperand)}</div>
       </div>
       <button className="span-two" onClick={() => dispatch({ type:ACTIONS.CLEAR })}>AC</button>
       <button onClick={() => dispatch({ type:ACTIONS.DELETE_DIGIT })}>DEL</button>
