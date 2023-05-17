ae2343b1a5ce26bca12d51c4a8b2a8f0231ffeb8
camaya21 <cindymamaya21@gmail.com>
2023-05-17 14:50:19 -0400

    Splitting operand on decimal

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -117,6 +117,15 @@ function evaluate ({ currentOperand, previousOperand, operation}) {
   return computation.toString()
 }
 
+const INTEGER_FORMATTER = new Intl.NumberFormat("en-us", {
+  maximumFractionDigits: 0,
+})
+
+function formatOperand(operand) {
+  if (operand == null) return
+  const [integer, decimal] = operand.split('.')
+}
+
 function App() {
   const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(reducer, {})
 
